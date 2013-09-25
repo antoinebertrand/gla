429195b063d3d8b83014719c67e07d215cd1c9a7
antoinebertrand <antoine.turbin@gmail.com>
2013-09-25 15:13:50 +0200

    Premier commit du tp1

---
+++ tp1/tpmaven/src/main/java/fr/istic/master2/tpmaven/App.java
@@ -0,0 +1,14 @@
+package fr.istic.master2.tpmaven;
+
+/**
+ * Hello world!
+ * youpi des commentaires !!!!!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int i;
+    	System.out.println( "Hello World!" );
+    }
+}
